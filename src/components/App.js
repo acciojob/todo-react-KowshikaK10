5c3cbae2be33f3b0970a38cebf559f110ed535b1
KowshikaK10 <kowshi010101@gmail.com>
2024-09-02 10:57:45 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todoVal,setTodoVal]=useState('');
+  const[todos,setTodos]=useState([]);
+  function handleAddtodo(){
+    setTodos([...todos,todoVal]);
+    setTodoVal(""); 
+  }
+  function handleDelete(index){
+    const newTodo=todos.filter((_, i) => i !== index);
+    setTodos(newTodo)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>TO-DO LIST</h1>
+        <input type="text"  value={todoVal} onChange={(e)=>setTodoVal(e.target.value)}/>
+        <button onClick={handleAddtodo}>Add to-do</button>
+        <ul>
+          {todos.map((todo,ind)=>(
+            <li key={ind}>{todo}
+            <button onClick={()=>handleDelete(ind)}>Delete</button>
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin= require('html-webpack-plugin');
-module.exports = {
-    entry: './src/index.js',
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-    output: {
-        path: path.join(__dirname,"/dist"),
-        filename: "index_bundle.js",
-    },
-    module:{
-        rules: [
-            {
-                test: /\.js$|\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ]
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ],
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    }
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'index_bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Clean the output directory before emit
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your HTML file
+      inject: 'body', // Inject scripts at the end of the body
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    port: 8080,
+    hot: true,
+  },
 };
